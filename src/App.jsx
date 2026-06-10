e95c569e33f6c3e2cda9aa94385d1182e67ef049
Jamshid <jamshidabduraxmonov164@gmail.com>
2026-06-10 19:28:00 +0400

    feat: implement visual marker position for any clicked coordinates and display them in a popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { MapContainer } from 'react-leaflet/MapContainer'
 import { TileLayer } from 'react-leaflet/TileLayer'
 import { useMap } from 'react-leaflet/hooks'
-import { Marker, Popup, useMapEvents } from 'react-leaflet'
+import { Marker, CircleMarker, Popup, useMapEvents } from 'react-leaflet'
 import {useRef, useState, useEffect} from 'react';
 
 
@@ -24,7 +24,7 @@ export default function App() {
 
   const [coordinates, setCoordinates] = useState([25.1201526, 55.3654066]);
   const [smoothCoordinates, setSmoothCoordinates] = useState([]);
-  const [clickedPosition, setClickedPosition ] = useState([]);
+const [clickedPosition, setClickedPosition ] = useState({lat: 25.1201526, lng: 55.3654066});
 
   console.log("render");
 
@@ -121,11 +121,19 @@ export default function App() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
         />
-        <Marker position={coordinates}>
+        <CircleMarker center={coordinates}>
             <Popup>
               A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
-        </Marker>
+        </CircleMarker>
+
+
+     <Marker position={clickedPosition}>
+                <Popup>
+                    The Coordinates: {clickedPosition.lat}, {clickedPosition.lng}
+                </Popup>
+      </Marker>
+       
 
         <ClickTracker setClickedPosition={setClickedPosition} />
     </MapContainer>
